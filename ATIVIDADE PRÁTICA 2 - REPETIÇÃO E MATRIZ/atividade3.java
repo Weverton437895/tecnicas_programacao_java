a7fc74f0f6b1358d07c4cecc61807c18d9579c42
Weverton437895 <wevertontevez0@gmail.com>
2025-08-15 23:24:06 -0300

    Update atividade3.java

--- ATIVIDADE PRÁTICA 2 - REPETIÇÃO E MATRIZ/atividade3.java
+++ ATIVIDADE PRÁTICA 2 - REPETIÇÃO E MATRIZ/atividade3.java
@@ -1,4 +1,39 @@
+import java.util.Scanner;
 
 public class atividade3 {
-
+	
+	public static void main(String[] args) {
+		
+		   Scanner entrada = new Scanner(System.in);
+		   
+	       System.out.print("Digite o número de linhas: ");
+	       int linhas = entrada.nextInt();
+	       
+	       System.out.print("Digite o número de colunas: ");
+	       int colunas = entrada.nextInt();
+	       
+	       int[][] matriz = new int[linhas][colunas];
+	       
+	       System.out.println("Digite os elementos da matriz:");
+	       
+	       for (int i = 0; i < linhas; i++) {
+	           for (int j = 0; j < colunas; j++) {
+	        	   
+	               System.out.printf("Elemento [%d][%d]: ", i, j);
+	               matriz[i][j] = entrada.nextInt();
+	           }
+	       }
+	       
+	       System.out.println("\nMatriz Transposta:");
+	       
+	       for (int i = 0; i < colunas; i++) {
+	           for (int j = 0; j < linhas; j++) {
+	        	   
+	               System.out.print(matriz[j][i] + " ");
+	           }
+	           
+	           System.out.println();
+	       }
+	       entrada.close();
+	}
 }
