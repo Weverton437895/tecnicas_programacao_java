d8e75821d086be92136af4af0b69d8b4be114461
Weverton <wevertontevez0@gmail.com>
2025-10-21 12:31:38 -0300

    Update Main2.java

--- ATIVIDADE PRÁTICA 7 - HERANÇA/view/Main2.java
+++ ATIVIDADE PRÁTICA 7 - HERANÇA/view/Main2.java
@@ -9,17 +9,16 @@ public class Main2 {
 	public static void main(String args[]) {
 		
 	
-		  double valorProduto = 100.0;
+		double valorProduto = 100.0;
 
-		  	PagamentoDinheiro pagamentoDinheiro = new PagamentoDinheiro(valorProduto);
-	        System.out.println("Pagamento em Dinheiro");
-	        System.out.println(pagamentoDinheiro.emitirRecibo());
+		PagamentoDinheiro pagamentoDinheiro = new PagamentoDinheiro(valorProduto);
+	    System.out.println("Pagamento em Dinheiro");
+	    System.out.println(pagamentoDinheiro.emitirRecibo());
 
-	        System.out.println();
+	    System.out.println();
 
-	        PagamentoCartao pagamentoCartao = new PagamentoCartao(valorProduto);
-	        System.out.println("Pagamento com Cartão");
-	        System.out.println(pagamentoCartao.emitirRecibo());
-	
+	    PagamentoCartao pagamentoCartao = new PagamentoCartao(valorProduto);
+	    System.out.println("Pagamento com Cartão");
+	    System.out.println(pagamentoCartao.emitirRecibo());
 	}
 }
